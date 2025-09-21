d2fa142541510a62ec6523b004c2193e8a9dd07c
Manu-Crack <manuelito_champeon2@hotmail.com>
2025-09-21 08:05:18 -0500

    Update db.py

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import pyodbc
 # Cadena de conexión fija para tu caso
 conn_str = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=DESKTOP-7S23QS1\\SQLEXPRESS2017;"
-    "DATABASE=flaskContacto;"
-    "UID=sa;"
-    "PWD=Ratas1234"
+    "SERVER=;"
+    "DATABASE=;"
+    "UID=;"
+    "PWD="
 )
 
 def get_connection():
